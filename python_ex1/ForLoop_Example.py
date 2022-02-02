36ec5971fccef802bf97ad11db7af8fc51165315
Will Kellum <wtk75435@uga.edu>
2022-02-02 12:50:30 -0500

    sending diy files

---
+++ python_ex1/ForLoop_Example.py
@@ -0,0 +1,7 @@
+test_list=[3,4,5,6,7,8] #holds numbers
+
+for list_item in test_list:
+    print(list_item)
+
+for i in range(0,len(test_list)): #len counts the number of variables (ie len(test_list)=6)
+    print(i, test_list[i]) #print the item number and print the variable
---
+++ python_ex1/app.py
@@ -0,0 +1,10 @@
+print("Go Dawgs")
+the_score=33.18 #using underscores are refered to as snake lettering
+print(the_score) # comment symbol
+"comment symbols"
+test_list=[1,2,3,4,5]
+for i in range (0, len(test_list)) :
+    print(i, test_list[i])
+
+for item in test_list:
+    print(item)
